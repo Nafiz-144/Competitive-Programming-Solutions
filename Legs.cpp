b25f1f39aacca6ed4d754e955ce1e9fba718f849
Nafiz144 <nafizahamed0464@gmail.com>
2024-08-17 17:13:53 +0600

    Tody

--- Legs.cpp
+++ Legs.cpp
@@ -9,13 +9,7 @@ int main()
     {
         int n;
         cin >> n;
-        int c1 = 0, c2 = 0, r = 0;
-
-        if (n == 2 && n == 4)
-        {
-            c1++;
-        }
-        cout << c1;
+        cout << (n + 2) / 4 << endl;
     }
 
     return 0;

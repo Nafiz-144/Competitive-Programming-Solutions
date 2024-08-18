c99d952efa91858bdcf06bd0752c3acb7a360c97
Nafiz144 <nafizahamed0464@gmail.com>
2024-08-19 00:13:27 +0600

    NEW

---
+++ Not_Quite_Latin_Square.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        char a, b, c;
+        for (int i = 0; i < 3; ++i)
+        {
+
+            cin >> a >> b >> c;
+        }
+        }
+    return 0;
+}

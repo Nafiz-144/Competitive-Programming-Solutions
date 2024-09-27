823f5d4d26a04a5768bb6f87cb661acb5df8c286
Nafiz144 <nafizahamed0464@gmail.com>
2024-09-27 19:14:22 +0600

    27-09-24

--- Problem_Generator.cpp
+++ Problem_Generator.cpp
@@ -2,34 +2,24 @@
 #include <string>
 #include <algorithm>
 using namespace std;
-
 void solve()
 {
     int n, m;
     cin >> n >> m;
     string a;
     cin >> a;
-
     int ans = 0;
-
-    // Check for each difficulty level 'A' to 'G'
-    for (char ch = 'A'; ch <= 'G'; ++ch)
+    for (char ch = 'A'; ch < 'H'; ++ch)
     {
-        // Count the occurrences of 'ch' in string 'a'
-        int count = count(a.begin(), a.end(), ch);
-        // If the count is less than m, add the deficit to 'ans'
-        ans += max(0, m - count);
+        ans += max(0, m - count(a.begin(), a.end(), ch));
     }
-
-    // Output the result
     cout << ans << endl;
 }
-
 int main()
 {
     int t;
     cin >> t;
-    while (t--)
+    for (int i = 0; i < t; ++i)
     {
         solve();
     }

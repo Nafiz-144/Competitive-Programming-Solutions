4400b75bc5777d0790918d11ce6006099897d37e
Nafiz144 <nafizahamed0464@gmail.com>
2024-10-14 20:55:26 +0600

    14-10-24

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,43 +1,45 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main()
-{
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
     int t;
     cin >> t;
 
-    while (t--)
-    {
+    while (t--) {
         int n, r;
         cin >> n >> r;
 
         vector<int> a(n);
-        for (int i = 0; i < n; i++)
-        {
+        for (int i = 0; i < n; i++) {
             cin >> a[i];
         }
 
         sort(a.begin(), a.end(), greater<int>());
 
-        int happyPeople = 0;
-        for (int i = 0; i < n; i++)
-        {
-            if (a[i] >= 2)
-            {
-                happyPeople += 2;
-                a[i] -= 2;
-            }
-            if (a[i] > 0 && r > 0)
-            {
-                happyPeople++;
-                r--;
+        int happy = 0;
+        int remainingSeats = 2 * r;
+
+        for (int i = 0; i < n; i++) {
+            if (a[i] == 1) {
+                happy++;
+                remainingSeats -= 1;
+            } else {
+                if (remainingSeats >= 2) {
+                    happy += 2;
+                    remainingSeats -= 2;
+                } else {
+                    happy++;
+                    remainingSeats -= 1;
+                }
             }
         }
 
-        cout << happyPeople << endl;
+        cout << happy << endl;
     }
 
     return 0;

c967a69096be3675e27e9664683e47d44258d3a1
Nafiz144 <nafizahamed0464@gmail.com>
2025-08-08 15:21:35 +0600

    s

---
+++ Merge_Sorted_Array.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int m, n, i, j;
+    cin >> m >> n;
+    vector<int> a, b, c;
+    for (auto i = 0; i < a.size(); i++)
+    {
+        cin >> a[i];
+    }
+    for (auto k = 0; k < m + n; k++)
+    {
+        c = a[i] + b[i];
+    }
+
+    cout << c(begin().end()) << endl;
+
+    return 0;
+}
